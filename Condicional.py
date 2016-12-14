0b2a7056343a500e8a6087fa3f4094227831afda
ELIACER PAPI <fernandez.18.eliacer@gmail.com>
2016-12-14 09:32:37 -0500

    This is the first commit

---
+++ Condicional.py
@@ -0,0 +1,17 @@
+a = int(input("First number: "))
+b = int(input("First number: "))
+
+#Condicional if
+
+if a > b:
+    print("El primer numero es mayor.")
+else:
+    print("El segundo numero es mayor.")
+
+
+#Bucle while, do while
+
+while a>b:
+    print("A es mayor que b")
+    b = b+1
+
---
+++ suma.py
@@ -0,0 +1,6 @@
+
+print("Suma de dos numeros")
+a = int(input("Primer Numero: "))
+b = int(input("Segundo Numero: "))
+c = a + b
+print("La suma es: ",c)
